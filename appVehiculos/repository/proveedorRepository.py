e2e5703340b2ac3fd2559e4e2740c6bd9f7a4526
alonsobox <alonsobox_14@hotmail.com>
2024-12-11 16:42:01 -0500

    Agregamos Consultas y modificaciones del SQL al Python del proveedorRepository

--- appVehiculos/repository/proveedorRepository.py
+++ appVehiculos/repository/proveedorRepository.py
@@ -4,3 +4,29 @@ class ProveedorRepository:
 
     def __init__(self):
         self.conexion = ConexionBaseDatos().getConection()
+
+    def listarProveedor(self):
+        cursor = self.conexion.cursor()
+        sql = "select cod_proveedor, nombre_proveedor, telefono_proveedor, stock_proveedor, direccion_proveedor from proveedor "
+        cursor.execute(sql)
+        return cursor.fetchall()
+    
+    def obtenerProveedor(self,codPro):
+        cursor= self.conexion.cursor()
+        sql = "select cod_proveedor, nombre_proveedor, telefono_proveedor, stock_proveedor, direccion_proveedor from proveedor WHERE cod_proveedor = '{}'".format(codPro)
+        cursor.execute(sql)
+        return cursor.fetchall()
+    
+    def insertarProveedor(self, objProveedor):
+        cursor = self.conexion.cursor()
+        sql = "INSERT INTO proveedor (nombre_proveedor, telefono_proveedor, stock_proveedor, direccion_proveedor) VALUES ('{}','{}','{}','{}')".format(objProveedor.nombrePro, objProveedor.telfPro, objProveedor.stockPro, objProveedor.direcPro)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
+
+    def actualizarProveedor(self,objProveedor):
+        cursor = self.conexion.cursor()
+        sql = "UPDATE proveedor SET nombre_proveedor = '{}', telefono_proveedor = '{}', stock_proveedor = '{}', direccion_proveedor = '{}' WHERE cod_proveedor = '{}'".format(objProveedor.nombrePro, objProveedor.telfPro, objProveedor.stockPro, objProveedor.direcPro, objProveedor.codPro)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
