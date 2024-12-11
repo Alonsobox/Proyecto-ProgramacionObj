0dae9842087ef27f48b1e802c2db7181a36ca136
alonsobox <alonsobox_14@hotmail.com>
2024-12-11 17:12:21 -0500

    Agregamos Consultas y modificaciones del SQL al Python del marcaRepository

--- appVehiculos/repository/marcaRepository.py
+++ appVehiculos/repository/marcaRepository.py
@@ -5,4 +5,28 @@ class MarcaRepository:
     def __init__(self):
         self.conexion = ConexionBaseDatos().getConection()
 
+    def listarMarca(self):
+        cursor = self.conexion.cursor()
+        sql = "select cod_marca, nombre_marca, stock_marca from marca"
+        cursor.execute(sql)
+        return cursor.fetchall()
     
+    def obtenerMarca(self, codMar):
+        cursor = self.conexion.cursor()
+        sql = "select cod_marca, nombre_marca, stock_marca from marca WHERE cod_marca = '{}'".format(codMar)
+        cursor.execute(sql)
+        return cursor.fetchone()
+    
+    def insertarMarca(self, objMarca):
+        cursor = self.conexion.cursor()
+        sql = "INSERT INTO marca (nombre_marca, stock_marca) VALUES ('{}','{}')".format(objMarca.nombreMar, objMarca.stockMar)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
+
+    def actualizarMarca(self, objMarca):
+        cursor = self.conexion.cursor()
+        sql = "UPDATE marca SET nombre_marca = '{}', stock_marca = '{}' WHERE cod_marca = '{}'".format(objMarca.nombreMar, objMarca.stockMar, objMarca.codMar)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
