149c9819489ce75fb273adda4b94052db012acd1
alonsobox <alonsobox_14@hotmail.com>
2024-12-11 15:14:47 -0500

    Agregamos Consultas y modificaciones del SQL al Python del vendedorRepository

--- appVehiculos/repository/vendedorRepository.py
+++ appVehiculos/repository/vendedorRepository.py
@@ -11,3 +11,22 @@ class VendedorRepository:
         cursor.execute(sql)
         return cursor.fetchall()
         
+    def obtenerVendedor(self,codPer):
+        cursor = self.conexion.cursor()
+        sql = "select * from vendedor".format(codPer)
+        cursor.execute(sql)
+        return cursor.fetchone()
+    
+    def insertarVendedor(self,objVendedor):
+        cursor= self.conexion.cursor()
+        sql = "INSERT INTO vendedor (cod_vendedor,nombre_vendedor,apellido_vendedor,sueldo_vendedor,ventas_vendedor) VALUES ('{}','{}','{}','{}','{}')".format(objVendedor.codPer, objVendedor.nombrePer, objVendedor.apelliPer, objVendedor.sueldoVen, objVendedor.ventasVen)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
+
+    def actualizarVendedor(self,objVendedor):
+        cursor = self.conexion.cursor()
+        sql = "UPDATE vendedor SET nombre_vendedor = '{}', apellido_vendedor = '{}', sueldo_vendedor = '{}', ventas_vendedor = '{}' WHERE cod_vendedor = '{}' ".format(objVendedor.nombrePer, objVendedor.apelliPer, objVendedor.sueldoVen, objVendedor.ventasVen,objVendedor.codPer)
+        cursor.execute(sql)
+        self.conexion.commit()
+        cursor.close()
